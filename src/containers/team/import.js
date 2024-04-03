7c22524eb891d54935b20171ac64bd19b147f65f
ShantnuS <shantnu@outlook.com>
2024-04-03 18:37:29 +0100

    website v1

--- src/containers/team/import.js
+++
@@ -1,9 +0,0 @@
-import johan from '../../assets/johan.jpg';
-import shan from '../../assets/shan.jpg';
-import marius from '../../assets/marius.jpg';
-
-export {
-    johan,
-    shan,
-    marius,
-}
